4a789a231272e38f80ad3139502b8b0ac67b003b
mcctrammit <mcctrammit@gmail.com>
2026-04-06 12:39:11 -0300

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,6 +1,25 @@
 export default async function handler(req, res) {
-  res.status(200).json({
-    ok: true,
-    message: 'Proxy funcionando'
-  });
+  try {
+    const url = `https://${process.env.VTEX_ACCOUNT}.vtexcommercestable.com.br/api/oms/pvt/orders`;
+
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: {
+        'X-VTEX-API-AppKey': process.env.VTEX_APP_KEY,
+        'X-VTEX-API-AppToken': process.env.VTEX_APP_TOKEN,
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+    });
+
+    const data = await response.json();
+
+    res.status(response.status).json(data);
+
+  } catch (error) {
+    res.status(500).json({
+      ok: false,
+      error: error.message
+    });
+  }
 }
